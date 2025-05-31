f5f00413ac6a4d589334fa178140fd9ec539ee89
Tim Salikhov <tim.dragonoid@gmail.com>
2025-06-01 01:36:21 +0300

    Added plugins for connector

--- post-startup.sh
+++ post-startup.sh
@@ -1,2 +1,10 @@
 docker exec -it users_service alembic upgrade head
 docker exec -it products_service alembic upgrade head
+
+docker exec -i connect curl -X POST -H "Content-Type: application/json" \
+  --data @/tmp/configs/elasticsearch-sink.json \
+  http://localhost:8083/connectors
+
+docker exec -i connect curl -X POST -H "Content-Type: application/json" \
+  --data @/tmp/configs/postgres-source.json \
+  http://localhost:8083/connectors
