d0a663270a7aa4e335bfdd79f3e9ca6a7c39b4d8
Tim Salikhov <tim.dragonoid@gmail.com>
2025-06-03 11:57:52 +0300

    reviews_service completed

--- reviews_service/src/main.py
+++ reviews_service/src/main.py
@@ -1,33 +1,25 @@
 from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
 from prometheus_client import make_asgi_app
-from src.config import config
 from src.router import router
 from src.middleware import LoggingMiddleware
 
 openapi_tags = [
-    {'name': 'unauthorized', 'description': 'Does not require authorization'},
-    {'name': 'authorized', 'description': 'Requires proper authorization'}
+    {"name": "unauthorized", "description": "Operations for everyone"},
+    {"name": "authorized", "description": "Operations for authorized users only"},
+    {"name": "service", "description": "Service endpoints"}
 ]
 
 app = FastAPI(
     openapi_tags=openapi_tags,
-    docs_url='/docs'
+    docs_url='/reviews_docs',
+    openapi_url='/reviews_openapi.json'
 )
 
 # Добавляем middleware для логирования
 app.add_middleware(LoggingMiddleware)
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[config.GATEWAY_URL],
-    allow_credentials=True,
-    allow_methods=['GET', 'POST', 'PUT', 'DELETE'],
-    allow_headers=['*']
-)
-
 # Добавляем эндпоинт для метрик Prometheus
 metrics_app = make_asgi_app()
 app.mount("/metrics", metrics_app)
 
-app.include_router(router, prefix='/api/reviews')
+app.include_router(router, prefix='/reviews')
